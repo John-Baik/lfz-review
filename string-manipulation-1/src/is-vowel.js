ca12544df6acfb1b642167d4ac7424a705a6bc32
John-Baik <johnwbaik@gmail.com>
2022-05-03 16:33:01 +0000

    Completed string-manipulation-1

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,1 +1,15 @@
 /* exported isVowel */
+function isVowel(character) {
+  const vowels = {
+    a: true,
+    e: true,
+    i: true,
+    o: true,
+    u: true
+  };
+  if (vowels[character.toLowerCase()]) {
+    return true;
+  } else {
+    return false;
+  }
+}
