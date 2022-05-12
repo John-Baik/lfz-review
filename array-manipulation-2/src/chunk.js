3f73722143709faf5e1cbc2c06106fa18b201c79
John Baik <johnwbaik@gmail.com>
2022-05-11 23:58:53 -0700

    Completed chunk

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,35 +1,33 @@
 /* exported chunk */
 function chunk(array, size) {
-const newArray = [];
-let miniArray = [];
-let count = 1;
-for (let i = 0; i < array.length; i++) {
-  if (count === size) {
-    miniArray.push(array[i]);
-    newArray.push(miniArray);
-    miniArray = [];
-    count = 1;
-  } else if (i === array.length - 1) {
-    miniArray.push(array[i]);
-    newArray.push(miniArray);
-  } else {
-    miniArray.push(array[i]);
-    count++;
+  let newArray = [];
+  let i = 0;
+  while (i < array.length) {
+    newArray.push(array.slice(i, i + size));
+    i += size;
   }
+  return newArray;
 }
-return newArray;
 
 
 
 
-//   let newArray = [];
-//   let i = 0;
-//   while (i < array.length) {
-//     let miniArray = [];
-//     miniArray.push(array.slice(i, i + size));
+
+// let newArray = [];
+// let miniArray = [];
+// let count = 1;
+// for (let i = 0; i < array.length; i++) {
+//   if (count === size) {
+//     miniArray.push(array[i]);
 //     newArray.push(miniArray);
-//     i += size;
-//     console.log('newArray: ', newArray)
+//     miniArray = [];
+//     count = 1;
+//   } else if (i === array.length - 1) {
+//     miniArray.push(array[i]);
+//     newArray.push(miniArray);
+//   } else {
+//     miniArray.push(array[i]);
+//     count++;
 //   }
-//   return newArray;
 // }
+// return newArray;
