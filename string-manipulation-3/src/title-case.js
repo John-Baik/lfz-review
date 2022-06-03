a4df12a8ff75b12a1291bd63d5dc09591f7b5271
John Baik <johnwbaik@gmail.com>
2022-06-02 20:49:54 -0700

    Updated title-case

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,18 +1,25 @@
 /* exported titleCase */
 function titleCase(string) {
-  const splitArray = string.split(' ');
+  const splitArray = string.split(':  ');
   for (let i = 0; i < splitArray.length; i++) {
+    let right;
+    if (splitArray[i - 1]) {
+       right = splitArray[i - 1].length - 1;
+    }
+
     const lowerCase = splitArray[i].toLowerCase().substring(1);
     const firstLetter = splitArray[i][0].toUpperCase();
     const finalString = firstLetter + lowerCase;
     if (finalString === 'Javascript') {
       splitArray[i] = 'JavaScript';
+    } else if (finalString === 'Javascript:') {
+      splitArray[i] = 'JavaScript:';
     } else if (finalString === 'Api') {
       splitArray[i] = 'API';
-    } else {
+    } else if (i === 0 || splitArray[i - 1][right] === ':' || splitArray[i].length >= 4) {
       splitArray[i] = finalString;
     }
   }
-  console.log(splitArray.join(' '));
+  console.log(splitArray.join(':'));
   return splitArray.join(' ');
 }
