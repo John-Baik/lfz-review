b6e805bda051d571dedbbd86f495ba30b98e42c5
John Baik <johnwbaik@gmail.com>
2022-06-25 17:02:42 -0700

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName =  'John' + ' ' + 'Baik';
+var isCool = true;
+var totalPets = 2;
+
+var unknown = null;
+var never;
+console.log(`fullName: ${fullName}`);
+console.log(`isCool: ${isCool}`);
+console.log(`totalPets: ${totalPets}`);
+
+console.log(`typeof fullName: ${typeof fullName}`);
+console.log(`typeof isCool: ${typeof isCool}`);
+console.log(`typeof totalPets: ${typeof totalPets}`);
+
+console.log(`unknown ${unknown}`);
+console.log(`typeof unknown: ${unknown}`);
+
+console.log(never)
+console.log(`typeof unknown: ${never}`);
